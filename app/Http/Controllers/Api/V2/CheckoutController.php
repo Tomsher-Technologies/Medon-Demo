0bf5658264a36367cb2aeaf29a47669bdcbd62a2
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2025-03-08 13:48:09 +0400

    test auto assign

--- app/Http/Controllers/Api/V2/CheckoutController.php
+++ app/Http/Controllers/Api/V2/CheckoutController.php
@@ -714,6 +714,16 @@ class CheckoutController extends Controller
 
     public function successAppPayment(Request $request, NearestShopService $nearestShopService){
 
+        $order = Order::where('code','=',$request->order_id)->firstOrFail();
+        $userLatitude = json_decode($order->shipping_address)->latitude ??  '';
+        $userLongitude = json_decode($order->shipping_address)->longitude ??  '';
+
+        if($userLatitude != '' && $userLongitude != ''){
+            $nearestShopService->assignNearestShop($order, $userLatitude, $userLongitude, 10);
+        }
+
+die;
+
         $encResponse = $request->encResp;          //This is the response sent by the CCAvenue Server
         $rcvdString = decryptCC($encResponse,env('CCA_WORKING_KEY')); //Crypto Decryption used as per the specified working key.
         $order_status = $order_code = $tracking_id = "";
